43bf0ab3512b9e03d04c2ecd6cd0875621005229
Julia Wu <julia_wu@brown.edu>
2016-01-04 18:50:28 -0200

    add merge two sorted lists

---
+++ sorting-searching/merge-two-sorted-lists.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        pointer = curr = ListNode(0) 
+        while l1 and l2:    
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+                curr = curr.next
+            elif l2.val < l1.val:
+                curr.next = l2
+                l2 = l2.next
+                curr = curr.next
+        if l1:
+            curr.next = l1
+        if l2:
+            curr.next = l2  
+        return pointer.next
