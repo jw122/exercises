de196329584779427e5a1cd6ef456aac1ffc7ec0
Julia Wu <julia_wu@brown.edu>
2016-08-03 00:03:08 -0700

    created list folder and added one question

---
+++ lists/merge_two_sorted.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        new_list = ListNode(0)
+        tmp = new_list
+        while l1 and l2:
+            if l1.val <= l2.val:
+                tmp.next = l1
+                l1 = l1.next
+                tmp = tmp.next
+            else:
+                tmp.next = l2
+                l2 = l2.next
+                tmp = tmp.next
+        
+        if l1:
+            tmp.next = l1
+
+        elif l2:
+            tmp.next = l2
+
+        return new_list.next
